180e6e38982ecbe76d0591c4055ca790123abdad
Michael Jensen <emjay1988@gmail.com>
2014-04-18 16:50:57 -0700

    Add schema matcher from dev project

---
+++ lib/json-schema-rspec/matchers/json_schema_matcher.rb
@@ -0,0 +1,55 @@
+require 'rspec'
+require 'json-schema'
+
+module JSON
+  module SchemaMatchers
+    RSpec.configure do |c|
+      c.add_setting :json_schemas, default: {}
+    end
+
+    class MatchJsonSchemaMatcher
+      def initialize(schema_name)
+        @schema_name = schema_name
+      end
+
+      def matches?(actual)
+        @actual = actual
+        schema = schema_for_name(@schema_name)
+        if schema.nil?
+          @errors = [
+            "No schema defined for #{@schema_name}",
+            "Add a line to your RSpec.configure block to define the schema:",
+            "  RSpec.configure do |config|",
+            "    config.json_schemas[:my_remote_schema] = 'path/to/schema'",
+            "    config.json_schemas[:my_inline_schema] = '{\"json\": \"schema\"}'",
+            "  end"]
+          return false
+        end
+        @errors = JSON::Validator.fully_validate(schema_for_name(@schema_name), @actual)
+
+        if @errors.any?
+          @errors.unshift("Expected JSON object to match schema identified by #{@schema_name}, #{@errors.count} errors in validating")
+          return false
+        else
+          return true
+        end
+      end
+
+      def failure_message
+        @errors.join("\n")
+      end
+
+      def failure_message_when_negated
+        "Expected JSON object not to match schema identified by #{@schema_name}"
+      end
+
+      def schema_for_name(schema)
+        RSpec.configuration.json_schemas[schema]
+      end
+    end
+
+    def match_json_schema(schema_name)
+      MatchJsonSchemaMatcher.new(schema_name)
+    end
+  end
+end
---
+++ lib/json-schema-rspec/version.rb
@@ -0,0 +1,7 @@
+module Json
+  module Schema
+    module Rspec
+      VERSION = "0.0.1"
+    end
+  end
+end
