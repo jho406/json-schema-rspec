7b623460e019717e4e9a86d21aead341758e9686
jgdreyes@gmail.com <jgdreyes@gamil.com>
2014-06-05 22:34:06 -0700

    A few updates and added specs
    - Updated confusing name in spec. Variable name matches spec better.
    - Added spec to explicitly test that the JSON::Validator is called
      correctly.

--- spec/json-schema-rspec/matchers/json_schema_matcher_spec.rb
+++ spec/json-schema-rspec/matchers/json_schema_matcher_spec.rb
@@ -2,28 +2,45 @@ require 'spec_helper'
 
 describe JSON::SchemaMatchers::MatchJsonSchemaMatcher do
   let(:valid_json)    { '"hello world"' }
-  let(:inline_schema) { '{"type": "string"}' }
   let(:invalid_json)  { '{"key": "value"}' }
+  let(:inline_schema) { '{"type": "string"}' }
 
   context 'without valid schema_name' do
+    let(:unconfigured_schema) { :unconfigured_schema }
     specify 'matches fail' do
-      expect(valid_json).not_to match_json_schema(:inline_schema)
+      expect(valid_json).not_to match_json_schema(unconfigured_schema)
     end
 
     specify 'strict matches fail' do
-      expect(valid_json).not_to match_json_schema(:inline_schema)
+      expect(valid_json).not_to match_json_schema(unconfigured_schema, strict: true)
     end
 
     specify 'assigns a failure message' do
-      matcher = match_json_schema(:inline_schema)
+      matcher = match_json_schema(unconfigured_schema)
       expect(matcher.matches?(valid_json)).to eq(false)
-      expect(matcher.failure_message).to match(/^No schema defined for inline_schema/)
+      expect(matcher.failure_message).to match(/^No schema defined for #{unconfigured_schema}/)
     end
   end
 
   context 'with valid schema_name' do
     before :each do
       RSpec.configuration.json_schemas[:inline_schema] = inline_schema
+    end
+
+    specify 'calls JSON::Validator' do
+      schema_value = inline_schema
+      json_value = valid_json
+      validation_options = {strict: true}
+      no_errors = []
+
+      expect(JSON::Validator).to receive(:fully_validate) do |schema_value, json_value, validation_options|
+        expect(schema_value).to eq(schema_value)
+        expect(json_value).to eq(json_value)
+        expect(validation_options).to eq(validation_options)
+        no_errors
+      end
+
+      expect(valid_json).to match_json_schema(:inline_schema, validation_options)
     end
 
     context 'finds a match' do
